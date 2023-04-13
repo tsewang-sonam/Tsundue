af5c753ea0e2ab428d10ca7d3ef5b277a9168acd
tsewangsonam <tsewangwhisky@gmail.com>
2023-04-13 12:30:46 -0700

    Initial Commit

---
+++ mockapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mockapp
+//
+//  Created by tsewang sonam on 4/13/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
